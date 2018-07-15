d78816bc60817df5e12ca6831080e9af823b4af3
Ashish <ash.ishu025g@gmail.com>
2018-07-15 14:23:55 +0530

    added a working code using react and redux which uses node API in turn

---
+++ src/app/actions/userAction.js
@@ -0,0 +1,59 @@
+//import { resolve } from "dns";
+//import { rejects } from "assert";
+import { setTimeout } from "timers";
+import { resolve } from "url";
+
+export function setName(name){
+    // return dispatch =>{
+    //     setTimeout( () => {
+    //         dispatch({
+    //             type:"SET_NAME",
+    //             payload:name
+    //         })
+    //     }
+    //     ,3000);
+    // }
+    // return{
+    //     type : "SET_NAME",
+    //     payload: new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             resolve(name);
+    //         },3000)
+    //     })        
+    // }; 
+    return{
+        type : "SET_NAME",
+        payload: {
+                promise: new Promise((resolve, reject) => { 
+                    fetch("http://localhost:9000/api/doctors", {
+                        method: 'GET',
+                        headers: {
+                          'Accept': 'application/json',
+                          'Content-Type': 'application/json'
+                        }
+                        //,body: JSON.stringify({ email: email, password: password})
+                    }
+                    ).then(                
+                        response => response.json(),
+                        error => console.log('An error occurred.', error)
+                    ).then(json => {
+                        // want to updatePath for the route here:
+                        //dispatch(updatePath('/'));
+                        resolve(json[6]["Desc"]);
+                    })
+                    .catch(error => {
+                        reject(error);
+                        //dispatch(error); -- promise Issue
+                        //https://github.com/pburtchaell/redux-promise-middleware/issues/24
+                    })
+                })
+            }       
+    }; 
+}
+
+export function setAge(number){
+    return{
+        type:"SET_AGE",
+        payload:number
+    }
+}
